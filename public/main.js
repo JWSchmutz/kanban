fedca84a1365093f503a2a33a6b9b63c6d679784
Carson <carsongoins2742@gmail.com>
2022-12-10 14:27:08 -0500

    Drag and drop

--- public/main.js
+++ public/main.js
@@ -7,6 +7,11 @@ function addTask() {
   //   document.getElementById("to-do").append(taskDiv);
 }
 
+// document.getElementById("jeremiah").addEventListener("click", ()=>{
+//     const element= document.getElementById("assigned-to");
+//     element.classList.add("jeremiah")
+// })
+
 document.getElementById("create-task-button").addEventListener("click", () => {
   fetch("/tasks", {
     method: "POST",
@@ -25,3 +30,22 @@ document.getElementById("create-task-button").addEventListener("click", () => {
       console.log("Request failure: ", error);
     });
 });
+
+function dragStart(event) {
+  event.dataTransfer.setData("Text", event.target.id);
+  document.getElementById("demo").innerHTML = "Started to drag the p element";
+}
+
+function dragEnd(event) {
+  document.getElementById("demo").innerHTML = "Finished dragging the p element.";
+}
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("Text");
+  event.target.appendChild(document.getElementById(data));
+}
