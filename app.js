e980a3feb1ca53913917cef63f2fde61063fa519
jwschmutz <jeremiah.schmutz@gmail.com>
2022-12-09 20:29:25 -0500

    initial setup

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const exphbs = require("express-handlebars");
+
+const db = require("./models");
+
+app.use(express.static("public"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+
+// const cupRoutes = require("./controllers/cupsController");
+// const matchupRoutes = require("./controllers/matchupsController");
+// app.use(cupRoutes);
+// app.use(matchupRoutes);
+
+const PORT = process.env.PORT || 5000;
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+});
