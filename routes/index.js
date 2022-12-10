318000f7b1fec38ee9d2a71642c1b63e5a392854
jwschmutz <jeremiah.schmutz@gmail.com>
2022-12-09 22:10:45 -0500

    backend setup 2nd phase - switched to mongoose

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const router = require("express").Router();
+const taskRoutes = require("./task");
+// user routes
+router.use("/", taskRoutes);
+// For anything else, render the html page
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+module.exports = router;
